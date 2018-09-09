adab35b4fc5936172c27227193bb49fe3ff81ba6
hamed shirbandi <hamed.shirbandi@gmail.com>
2018-09-09 18:48:50 +0430

    add caching

--- CorMon.Web/Controllers/HomeController.cs
+++ CorMon.Web/Controllers/HomeController.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Http;
 using System.Globalization;
 using CorMon.Application.Posts;
 using CorMon.Core.Enums;
+using CorMon.Core.Helpers;
+using RedisCache.Core;
+using System.Collections.Generic;
+using CorMon.Application.Posts.Dto;
 
 namespace CorMon.Web.Controllers
 {
@@ -14,15 +18,18 @@ namespace CorMon.Web.Controllers
         #region Fields
 
         private readonly IPostService _postService;
+        private readonly IRedisCacheService _redisCacheService;
+
         private int recordsPerPage;
 
         #endregion
 
         #region Ctor
 
-        public HomeController(IPostService postService)
+        public HomeController(IPostService postService, IRedisCacheService redisCacheService)
         {
             _postService = postService;
+            _redisCacheService = redisCacheService;
             recordsPerPage = 5;
         }
 
@@ -39,7 +46,13 @@ namespace CorMon.Web.Controllers
         /// </summary>
         public async Task<IActionResult> Index()
         {
-            var posts = await _postService.SearchAsync(page:0,recordsPerPage:recordsPerPage,term: "",taxonomyId:null,taxonomyType:null, publishStatus: PublishStatus.Publish, sortOrder: SortOrder.Desc);
+            var cacheKey = string.Format(CacheKeyTemplate.PostsSearchCacheKey, 0, recordsPerPage, null, null);
+
+            if (!_redisCacheService.TryGetValue(key: cacheKey, result: out IEnumerable<PostOutput> posts))
+            {
+                posts = await _postService.SearchAsync(page: 0, recordsPerPage: recordsPerPage, term: "", taxonomyId: null, taxonomyType: null, publishStatus: PublishStatus.Publish, sortOrder: SortOrder.Desc);
+                await _redisCacheService.SetAsync(key: cacheKey, data: posts, cacheTimeInMinutes: 60);
+            }
 
             return View(posts);
         }
